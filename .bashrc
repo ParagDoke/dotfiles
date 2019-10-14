b1fa732fec36588b4c2f2f3f80e2b8b160a7a151
Ubuntu <ubuntu@parag-dev.novalocal>
2019-10-14 08:52:12 +0000

    Append .bashrc instead of replacing

--- .bashrc
+++ .bashrc
@@ -1,1 +1,2 @@
+
 export PS1=$(echo ${PS1} | sed -e 's/w/W/g' -e 's/$/ /')
--- prep.sh
+++ prep.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -ex
-echo "Starting `basename $0`"
-cd $HOME
+echo "Starting $(basename $0)"
+cd ${HOME}
 for f in .bashrc .vimrc .tmux.conf; do
-    curl --remote-name "https://raw.githubusercontent.com/ParagDoke/dotfiles/master/$f"
+    curl --remote-name "https://raw.githubusercontent.com/ParagDoke/dotfiles/master/${f}"
 done
-echo "Finished `basename $0`"
+for f in .bashrc; do
+    curl "https://raw.githubusercontent.com/ParagDoke/dotfiles/master/${f}" >> ${f}
+done
+echo "Finished $(basename $0)"
