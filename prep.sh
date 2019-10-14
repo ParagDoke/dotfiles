24e19043698f13158ef736b8d12f96423a4e0a73
Parag Doke <parag.doke+techie@gmail.com>
2019-10-14 09:15:44 +0000

    Delete bashrc from 1st loop

--- prep.sh
+++ prep.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 echo "Starting $(basename $0)"
 cd ${HOME}
-for f in .bashrc .vimrc .tmux.conf; do
+for f in .vimrc .tmux.conf; do
     curl --remote-name "https://raw.githubusercontent.com/ParagDoke/dotfiles/master/${f}"
 done
 for f in .bashrc; do
